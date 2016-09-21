a1966b8dd473d8138a1670d9e39e9de894da1473
walston <nathanielwalston@gmail.com>
2016-09-21 16:35:34 -0700

    can input rgb now

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,47 @@
   function main(event) {
     var hex = doc.getElementById('hex');
     var rgb = doc.getElementById('rgb');
-    hex.addEventListener('change', update);
-    rgb.addEventListener('change', update);
+    hex.addEventListener('change', hexNormalize);
+    rgb.addEventListener('change', rgbNormalize);
   }
 
-  function update(event) {
-    var input = event.target.value;
-    if (input.length == 6) {
-      if (parseInt(input, 16) <= 0xffffff) {
-        set(input);
-      }
+  function hexNormalize(event) {
+    var input = event.target.value.replace(/\s*/g,"");
+    var regex = /[0-9a-fA-F]{6}/
+    if (!regex.test(input)) {
+      console.warn("Input invalid:", input);
+      return null;
+    }
+
+    var hexValue = regex.exec(input)[0];
+    if (hexValue && parseInt(hexValue, 16) <= 0xffffff) {
+      set(hexValue);
+    }
+  }
+
+  function rgbNormalize(event) {
+    var input = event.target.value.replace(/\s*/g,"");
+    if (!/^rgb\(\d{1,3},\d{1,3},\d{1,3}\)$/.test(input)) {
+      console.warn("Input invalid:", input);
+      return null;
+    }
+
+    var regex = /\d{1,3}/g;
+    var regexReturn;
+    var rgb = [];
+
+    while ((regexReturn = regex.exec(input)) !== null) {
+      if (regexReturn[0] > -1 && regexReturn[0] < 256)
+        rgb.push(regexReturn[0]);
+    }
+
+    var hex = hexize(rgb[0]) + hexize(rgb[1]) + hexize(rgb[2]);
+    set(hex);
+
+    function hexize(c) {
+      c = Number(c, 10).toString(16);
+      c = c.length < 2 ? "0" + c : c ;
+      return c;
     }
   }
 
